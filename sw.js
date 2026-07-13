c40211fc37c9b2be87255934615e949fa72dfc29
everton191 <everton191@users.noreply.github.com>
2026-07-13 06:42:41 -0300

    fix mobile panel scrolling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "simplifica-3d-v252-mobile-viewport-20260713";
+const CACHE_NAME = "simplifica-3d-v253-mobile-panel-scroll-20260713";
 const APP_FILES = [
   "./",
   "./index.html",
